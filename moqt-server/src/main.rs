5489d0525c72a761e1f7bf34e38bb2e27965dc34
y-i <y-i@users.noreply.github.com>
2023-10-02 12:20:12 +0900

    Add MOQT server endpoint

--- moqt-server/src/main.rs
+++ moqt-server/src/main.rs
@@ -1,29 +1,17 @@
-use anyhow::Result;
-use moqt::{constants::UnderlayType, MOQTConfig, MOQT};
+use anyhow::{Ok, Result};
+use moqt::{constants::UnderlayType, AuthCallbackType, MOQTConfig, MOQT};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    #[cfg(feature = "foo")]
-    {
-        println!("{}", exec_cb(add));
-    }
-    #[cfg(not(feature = "foo"))]
-    {
-        let mut config = MOQTConfig::new();
-        config.underlay = UnderlayType::WebTransport;
-        config.key_path = "./moqt/keys/key.pem".to_string();
-        config.cert_path = "./moqt/keys/cert.pem".to_string();
+    let mut config = MOQTConfig::new();
+    config.underlay = UnderlayType::WebTransport;
+    config.key_path = "./moqt/keys/key.pem".to_string();
+    config.cert_path = "./moqt/keys/cert.pem".to_string();
 
-        let moqt = MOQT::new(config);
-        moqt.start().await?;
-    }
+    // config.auth_callback = Some(|track_name: String, auth_payload: String, auth_callback_type: AuthCallbackType| {Ok(())});
+
+    let moqt = MOQT::new(config);
+    moqt.start().await?;
+
     Ok(())
-}
-
-fn exec_cb<T>(f: fn(u128, u128) -> T) -> T {
-    f(1, 2)
-}
-
-fn add(a: u128, b: u128) -> u128 {
-    a + b
 }
