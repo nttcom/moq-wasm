a61c6b2e64702edb414912aaf849d6b92b7f5e9b
yuki-uchida <yuki.u.nrnwr@gmail.com>
2024-04-04 17:41:11 +0900

    Chrome起動オプションからtmp　sessionを削除

--- scripts/start-localhost-test-chrome.sh
+++ scripts/start-localhost-test-chrome.sh
@@ -7,4 +7,4 @@
 # Get base 64 of scert cmd
 certbase64=$(eval "openssl x509 -pubkey -noout -in ./moqt-server-sample/keys/cert.pem | openssl rsa -pubin -outform der | openssl dgst -sha256 -binary | base64")
 
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --test-type --user-data-dir="/tmp/chrome_dev_session" --enable-features=SharedArrayBuffer --origin-to-force-quic-on=localhost:4433  --ignore-certificate-errors-spki-list=$certbase64 --use-fake-device-for-media-stream
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --test-type --enable-features=SharedArrayBuffer --origin-to-force-quic-on=localhost:4433  --ignore-certificate-errors-spki-list=$certbase64 --use-fake-device-for-media-stream
