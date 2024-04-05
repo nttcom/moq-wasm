89257c8fe5be5a7b4b0ed9636f186c8f57b7d862
tetta maeda <nu.maeda@gmail.com>
2024-04-05 15:32:51 +0900

    CLIコマンドでログレベルを指定

--- moqt-server-sample/src/main.rs
+++ moqt-server-sample/src/main.rs
@@ -1,12 +1,22 @@
 use anyhow::{Ok, Result};
+use clap::Parser;
 use moqt_server::{constants::UnderlayType, MOQTConfig, MOQT};
+
+#[derive(Debug, Parser)]
+struct Arg {
+    #[arg(short = 'l', long = "log", help = "Log Level", default_value = "DEBUG")]
+    log_level: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let args = Arg::parse();
+
     let mut config = MOQTConfig::new();
     config.underlay = UnderlayType::WebTransport;
     config.key_path = "./moqt-server-sample/keys/key.pem".to_string();
     config.cert_path = "./moqt-server-sample/keys/cert.pem".to_string();
+    config.log_level = args.log_level.to_string();
 
     // config.auth_callback = Some(|track_name: String, auth_payload: String, auth_callback_type: AuthCallbackType| {Ok(())});
 
