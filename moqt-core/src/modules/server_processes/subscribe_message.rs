745467856f4ed72df4741ff406b0936ce2ac0d5f
yuki-uchida <yuki.u.nrnwr@gmail.com>
2024-04-18 14:24:28 +0900

    subscribe_messageの処理をserver_processesに移動

---
+++ moqt-core/src/modules/server_processes/subscribe_message.rs
@@ -0,0 +1,55 @@
+use bytes::BytesMut;
+
+use crate::{
+    handlers::subscribe_handler::{subscribe_handler, SubscribeResponse},
+    messages::{moqt_payload::MOQTPayload, subscribe_request_message::SubscribeRequestMessage},
+    moqt_client::MOQTClientStatus,
+    MOQTClient, TrackManagerRepository,
+};
+use anyhow::{bail, Result};
+
+pub(crate) async fn process_subscribe_message(
+    payload_buf: &mut BytesMut,
+    client: &mut MOQTClient,
+    write_buf: &mut BytesMut,
+    track_manager_repository: &mut dyn TrackManagerRepository,
+) -> Result<(SubscribeResponse)> {
+    if client.status() != MOQTClientStatus::SetUp {
+        let message = String::from("Invalid timing");
+        tracing::error!(message);
+        bail!(message);
+    }
+
+    let subscribe_request_message = match SubscribeRequestMessage::depacketize(payload_buf) {
+        Ok(subscribe_request_message) => subscribe_request_message,
+        Err(err) => {
+            tracing::info!("{:#?}", err);
+            bail!(err.to_string());
+        }
+    };
+
+    let subscribe_result =
+        subscribe_handler(subscribe_request_message, client, track_manager_repository).await;
+
+    match subscribe_result {
+        Ok(subscribe_response) => match subscribe_response {
+            crate::modules::handlers::subscribe_handler::SubscribeResponse::Success(
+                subscribe_ok,
+            ) => {
+                subscribe_ok.packetize(write_buf);
+                Ok(SubscribeResponse::Success(subscribe_ok))
+            }
+            crate::modules::handlers::subscribe_handler::SubscribeResponse::Failure(
+                subscribe_error,
+            ) => {
+                subscribe_error.packetize(write_buf);
+                Ok(SubscribeResponse::Failure(subscribe_error))
+            }
+        },
+        Err(err) => {
+            // fix
+            tracing::info!("{:#?}", err);
+            bail!(err.to_string());
+        }
+    }
+}
