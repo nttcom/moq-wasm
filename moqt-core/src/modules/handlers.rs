3bca81d842eb532f432891f448d364d9f437fadf
tetta maeda <nu.maeda@gmail.com>
2024-08-07 20:56:02 +0900

    refactor: change filename

--- moqt-core/src/modules/handlers.rs
+++ moqt-core/src/modules/handlers.rs
@@ -1,6 +1,6 @@
 pub(crate) mod announce_handler;
 pub(crate) mod server_setup_handler;
-pub(crate) mod subscribe_request_handler;
-pub(crate) mod subscribe_response_handler;
+pub(crate) mod subscribe_handler;
+pub(crate) mod subscribe_ok_handler;
 pub(crate) mod unannounce_handler;
 pub(crate) mod unsubscribe_handler;
