b09854900ad136c26a468a9bdb71ea0e7ef46203
yuki-uchida <yuki.u.nrnwr@gmail.com>
2024-08-19 19:13:16 +0900

    revert LICENSE

--- scripts/start-localhost-test-chrome.sh
+++ scripts/start-localhost-test-chrome.sh
@@ -1,33 +1,10 @@
 #!/usr/bin/env bash
 
-
-# MIT License
-#
 # Copyright (c) Meta Platforms, Inc. and affiliates.
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
-# Modifications by NTT Communications Corporation(2024)
-# - modify the path of the cert.pem
-# - modify the path of the moqt-server-sample
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 # Get base 64 of scert cmd
 certbase64=$(eval "openssl x509 -pubkey -noout -in ./moqt-server-sample/keys/cert.pem | openssl rsa -pubin -outform der | openssl dgst -sha256 -binary | base64")
 
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --test-type --origin-to-force-quic-on=localhost:4433  --ignore-certificate-errors-spki-list=$certbase64 --use-fake-device-for-media-stream
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --test-type --origin-to-force-quic-on=localhost:4433 --ignore-certificate-errors-spki-list=$certbase64 --use-fake-device-for-media-stream
