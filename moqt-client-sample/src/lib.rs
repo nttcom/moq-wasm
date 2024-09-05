d74945607217893c77b73b63b1ed09771ff5de68
tetta maeda <nu.maeda@gmail.com>
2024-09-05 17:54:01 +0900

    fix: follow wasm-pack update

--- moqt-client-sample/src/lib.rs
+++ moqt-client-sample/src/lib.rs
@@ -26,12 +26,6 @@ use wasm_bindgen_futures::JsFuture;
 #[cfg(web_sys_unstable_apis)]
 use web_sys::ReadableStreamDefaultReader;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
