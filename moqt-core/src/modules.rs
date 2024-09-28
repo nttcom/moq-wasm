448466b1800bbd7c21bd92955d0dbcd977ce512c
tetta maeda <nu.maeda@gmail.com>
2024-09-28 14:54:07 +0900

    change crate name to use

--- moqt-core/src/modules.rs
+++ moqt-core/src/modules.rs
@@ -1,9 +1,8 @@
 pub mod constants;
-pub mod message_type;
+pub mod control_message_type;
 pub mod messages;
 pub mod moqt_client;
 pub mod send_stream_dispatcher_repository;
-pub mod stream_type;
 pub mod track_namespace_manager_repository;
 pub mod variable_bytes;
 pub mod variable_integer;
