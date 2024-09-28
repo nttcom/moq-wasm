98475b71f121ae57e4ff49bf7446dcd2eadfc9ec
tetta maeda <nu.maeda@gmail.com>
2024-09-28 15:05:52 +0900

    fix: stream type name

--- moqt-core/src/modules/data_stream_type.rs
+++ moqt-core/src/modules/data_stream_type.rs
@@ -1,6 +1,6 @@
 #[derive(Debug, PartialEq, Eq, TryFromPrimitive, IntoPrimitive, Clone)]
 #[repr(u8)]
-pub enum ObjectStreamType {
+pub enum DataStreamType {
     ObjectDatagram = 0x1,
     StreamHeaderTrack = 0x2,
     StreamHeaderSubgroup = 0x4,
