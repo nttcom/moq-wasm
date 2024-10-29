2b94194d7f9cc5b09de5aebd7029f2892bcb7912
tetter <117813816+tetter27@users.noreply.github.com>
2024-10-29 19:16:25 +0900

    Propagate announce if the namespace was subscribed by subscribers (#117)
    * add todo comment
    
    * refactor: add error handling
    
    * feat: announce propagate
    
    * refactor: error message
    
    * Support new messages relay feature (#120)
    
    * feat: relay new messages
    
    * feat: send subscribe namespace ok
    
    * Support client send/recv SUBSCRIBE_NAMESPACE (#121)
    
    * feat: send/recv subscribe_namespace
    
    * Merge branch 'impl/new_message_relay' into impl/client/subscribe_namespace
    
    * delete: unnecessary parameter
    
    * fix: handle multiple control message
    
    * feat: send announce ok message
    
    * refactor: send/recv log

--- moqt-server/src/modules/server_processes/announce_message.rs
+++ moqt-server/src/modules/server_processes/announce_message.rs
@@ -1,7 +1,9 @@
-use crate::modules::handlers::announce_handler::{announce_handler, AnnounceResponse};
+use crate::modules::handlers::announce_handler::announce_handler;
 use anyhow::{bail, Result};
 use bytes::BytesMut;
+use moqt_core::messages::control_messages::announce_error::AnnounceError;
 use moqt_core::pubsub_relation_manager_repository::PubSubRelationManagerRepository;
+use moqt_core::send_stream_dispatcher_repository;
 use moqt_core::{
     messages::{control_messages::announce::Announce, moqt_payload::MOQTPayload},
     MOQTClient,
@@ -12,7 +14,8 @@ pub(crate) async fn process_announce_message(
     client: &mut MOQTClient,
     write_buf: &mut BytesMut,
     pubsub_relation_manager_repository: &mut dyn PubSubRelationManagerRepository,
-) -> Result<AnnounceResponse> {
+    send_stream_dispatcher_repository: &mut dyn send_stream_dispatcher_repository::SendStreamDispatcherRepository,
+) -> Result<Option<AnnounceError>> {
     let announce_message = match Announce::depacketize(payload_buf) {
         Ok(announce_message) => announce_message,
         Err(err) => {
@@ -21,22 +24,23 @@ pub(crate) async fn process_announce_message(
         }
     };
 
-    let announce_response =
-        announce_handler(announce_message, client, pubsub_relation_manager_repository).await;
+    let result = announce_handler(
+        announce_message,
+        client,
+        pubsub_relation_manager_repository,
+        send_stream_dispatcher_repository,
+    )
+    .await;
 
-    match announce_response {
-        Ok(announce_response_message) => match announce_response_message {
-            AnnounceResponse::Success(ok_message) => {
-                ok_message.packetize(write_buf);
-                Ok(AnnounceResponse::Success(ok_message))
-            }
-            AnnounceResponse::Failure(err_message) => {
-                err_message.packetize(write_buf);
-                Ok(AnnounceResponse::Failure(err_message))
-            }
-        },
+    match result.as_ref() {
+        Ok(Some(announce_error)) => {
+            announce_error.packetize(write_buf);
+
+            result
+        }
+        Ok(None) => result,
         Err(err) => {
-            tracing::error!("{:#?}", err);
+            tracing::error!("announce_handler: err: {:?}", err.to_string());
             bail!(err.to_string());
         }
     }
