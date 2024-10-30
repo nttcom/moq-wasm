9895b14bb9702823ab86985c547d059cf2460077
tetter <117813816+tetter27@users.noreply.github.com>
2024-10-30 22:16:46 +0900

    Support stream per subgroup on server and client (#123)
    * feat: stream per subgroup
    
    * Merge branch 'draft-06' into impl/subgroup_stream

--- moqt-server/src/modules/server_processes.rs
+++ moqt-server/src/modules/server_processes.rs
@@ -3,6 +3,7 @@ pub(crate) mod announce_message;
 pub(crate) mod announce_ok_message;
 pub(crate) mod client_setup_message;
 pub(crate) mod stream_track_header;
+pub(crate) mod stream_track_subgroup;
 pub(crate) mod subscribe_error_message;
 pub(crate) mod subscribe_message;
 pub(crate) mod subscribe_namespace_message;
