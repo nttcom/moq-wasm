f452c7c26ab74467e57672228565083fe84541ef
tetter <117813816+tetter27@users.noreply.github.com>
2024-10-30 21:57:00 +0900

    Support SUBSCRIBE_ERROR handler (#113)
    * impl: update version to 06
    
    * fix: mark ANNOUNCE_ERROR
    
    * fix: wrong variable name
    
    * impl: subscribe_error packetize/depacketize
    
    * feat: handle subscribe failure
    
    * feat: handle subscribe error message
    
    * send/recv subscribe error on client
    
    * feat: subscribe error
    
    * fix: change default value to 06
    
    * fix: wrong item text
    
    * add field for 06
    
    * fix: typo
    
    * add comment
    
    * merge js from draft-06
    
    * Changed the method to create relay messages (#119)
    
    * refactor: create relay message
    
    * refactor: log timing
    
    * refactor: object header track

--- moqt-client-sample/src/lib.rs
+++ moqt-client-sample/src/lib.rs
@@ -476,26 +476,22 @@ impl MOQTClient {
     #[wasm_bindgen(js_name = sendSubscribeErrorMessage)]
     pub async fn send_subscribe_error_message(
         &self,
-        track_namespace: js_sys::Array,
-        track_name: String,
+        subscribe_id: u64,
         error_code: u64,
         reason_phrase: String,
     ) -> Result<JsValue, JsValue> {
         if let Some(writer) = &*self.control_stream_writer.borrow() {
-            let length = track_namespace.length();
-            let mut track_namespace_vec: Vec<String> = Vec::with_capacity(length as usize);
-            for i in 0..length {
-                let js_element = track_namespace.get(i);
-                let string_element = js_element
-                    .as_string()
-                    .ok_or_else(|| JsValue::from_str("Array contains a non-string element"))?;
-                track_namespace_vec.push(string_element);
-            }
+            // Find unused subscribe_id and track_alias automatically
+            let valid_track_alias = self
+                .subscription_node
+                .borrow_mut()
+                .create_valid_track_alias()
+                .unwrap();
             let subscribe_error_message = SubscribeError::new(
-                track_namespace_vec,
-                track_name,
+                subscribe_id,
                 SubscribeErrorCode::try_from(error_code as u8).unwrap(),
                 reason_phrase,
+                valid_track_alias,
             );
             let mut subscribe_error_message_buf = BytesMut::new();
             subscribe_error_message.packetize(&mut subscribe_error_message_buf);
@@ -1064,6 +1060,14 @@ impl SubscriptionNode {
             consumer.create_latest_subscribe_id_and_track_alias()
         } else {
             Err(anyhow::anyhow!("consumer is None"))
+        }
+    }
+
+    fn create_valid_track_alias(&self) -> Result<u64> {
+        if let Some(producer) = &self.producer {
+            producer.create_valid_track_alias()
+        } else {
+            Err(anyhow::anyhow!("producer is None"))
         }
     }
 
