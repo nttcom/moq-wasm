ac50c0c1051ed49fc9fc2d5ab041d4311b43374d
tetta maeda <nu.maeda@gmail.com>
2024-11-02 14:07:17 +0000

    change destination to online server

--- scripts/start-localhost-test-chrome.sh
+++ scripts/start-localhost-test-chrome.sh
@@ -7,4 +7,4 @@
 # Get base 64 of scert cmd
 certbase64=$(eval "openssl x509 -pubkey -noout -in ./moqt-server-sample/keys/cert.pem | openssl rsa -pubin -outform der | openssl dgst -sha256 -binary | base64")
 
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --test-type --origin-to-force-quic-on=localhost:4433 --ignore-certificate-errors-spki-list=$certbase64 --use-fake-device-for-media-stream
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --test-type --origin-to-force-quic-on=34.147.225.127:4433 --ignore-certificate-errors-spki-list=$certbase64 --use-fake-device-for-media-stream
