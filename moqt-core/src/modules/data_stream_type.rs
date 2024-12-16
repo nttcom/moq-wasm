36638dcd8f4878131ac31c6cae7d9700fe553099
tetter <117813816+tetter27@users.noreply.github.com>
2024-12-16 19:35:35 +0900

    Separate moqt-server/lib.rs (#134)
    * refactor: separate lib.rs
    
    * refactor: session_handler as class
    
    * fix: indicated items
    
    * separate fn in uni stream receiver (#135)

--- moqt-core/src/modules/data_stream_type.rs
+++ moqt-core/src/modules/data_stream_type.rs
@@ -1,6 +1,6 @@
 use num_enum::{IntoPrimitive, TryFromPrimitive};
 
-#[derive(Debug, PartialEq, Eq, TryFromPrimitive, IntoPrimitive, Clone)]
+#[derive(Debug, PartialEq, Eq, TryFromPrimitive, IntoPrimitive, Clone, Copy)]
 #[repr(u8)]
 pub enum DataStreamType {
     ObjectDatagram = 0x1,
--- moqt-server/src/modules.rs
+++ moqt-server/src/modules.rs
@@ -2,6 +2,8 @@ pub mod buffer_manager;
 pub(crate) mod logging;
 pub(crate) mod message_handlers;
 pub(crate) mod moqt_client;
+pub(crate) mod moqt_config;
 pub(crate) mod object_cache_storage;
 pub(crate) mod pubsub_relation_manager;
 pub(crate) mod send_stream_dispatcher;
+pub(crate) mod server_processes;
