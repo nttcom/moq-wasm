1a347d450b3581dd10b4565c580f4136269d1235
yuki-uchida <yuki.u.nrnwr@gmail.com>
2024-12-17 17:59:21 +0900

    implement subscribe_done

--- moqt-core/src/modules/messages/control_messages/subscribe_done.rs
+++ moqt-core/src/modules/messages/control_messages/subscribe_done.rs
@@ -1,11 +1,18 @@
-use crate::messages::moqt_payload::MOQTPayload;
+use crate::{
+    messages::moqt_payload::MOQTPayload,
+    variable_bytes::{
+        read_fixed_length_bytes_from_buffer, read_variable_bytes_from_buffer, write_variable_bytes,
+    },
+    variable_integer::{read_variable_integer_from_buffer, write_variable_integer},
+};
+use anyhow::{bail, Context, Result};
 use bytes::BytesMut;
 use num_enum::{IntoPrimitive, TryFromPrimitive};
 use serde::Serialize;
 use std::any::Any;
 
 #[derive(Debug, IntoPrimitive, TryFromPrimitive, Serialize, Clone, Copy, PartialEq)]
-#[repr(u8)]
+#[repr(u64)]
 pub enum StatusCode {
     Unsubscribed = 0x0,
     InternalError = 0x1,
@@ -47,17 +54,113 @@ impl SubscribeDone {
 }
 
 impl MOQTPayload for SubscribeDone {
-    fn depacketize(buf: &mut BytesMut) -> anyhow::Result<Self>
-    where
-        Self: Sized,
-    {
-        let subscribe_id = buf.get_u64();
+    fn depacketize(buf: &mut BytesMut) -> Result<Self> {
+        let subscribe_id = read_variable_integer_from_buffer(buf).context("subscribe id")?;
+        let status_code_u64 = read_variable_integer_from_buffer(buf).context("status code")?;
+        let status_code = StatusCode::try_from(status_code_u64).context("status code")?;
+        let reason_phrase =
+            String::from_utf8(read_variable_bytes_from_buffer(buf)?).context("reason phrase")?;
+        let content_exists =
+            match read_fixed_length_bytes_from_buffer(buf, 1).context("content_exists")?[0] {
+                0 => false,
+                1 => true,
+                _ => {
+                    // TODO: return Termination Error Code
+                    bail!("Invalid content_exists value: Protocol Violation");
+                }
+            };
+
+        let (final_group_id, final_object_id) = match content_exists {
+            true => {
+                let final_group_id =
+                    read_variable_integer_from_buffer(buf).context("final_group_id")?;
+                let final_object_id =
+                    read_variable_integer_from_buffer(buf).context("final_object_id")?;
+                (Some(final_group_id), Some(final_object_id))
+            }
+            false => (None, None),
+        };
+
+        Ok(SubscribeDone {
+            subscribe_id,
+            status_code,
+            reason_phrase,
+            content_exists,
+            final_group_id,
+            final_object_id,
+        })
     }
     fn packetize(&self, buf: &mut BytesMut) {
-        buf.put_u64(self.subscribe_id);
+        buf.extend(write_variable_integer(self.subscribe_id));
+        buf.extend(write_variable_integer(u64::from(self.status_code)));
+        buf.extend(write_variable_bytes(
+            &self.reason_phrase.as_bytes().to_vec(),
+        ));
+        buf.extend(u8::from(self.content_exists).to_be_bytes());
+        if self.content_exists {
+            buf.extend(write_variable_integer(self.final_group_id.unwrap()));
+            buf.extend(write_variable_integer(self.final_object_id.unwrap()));
+        }
     }
-    /// Method to enable downcasting from MOQTPayload to SubscribeError
+    /// Method to enable downcasting from MOQTPayload to SubscribeDone
     fn as_any(&self) -> &dyn Any {
         self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    mod success {
+        use crate::messages::{
+            control_messages::subscribe_done::{StatusCode, SubscribeDone},
+            moqt_payload::MOQTPayload,
+        };
+        use bytes::BytesMut;
+        #[test]
+        fn packetize() {
+            let subscribe_done = SubscribeDone::new(
+                0,
+                StatusCode::Unsubscribed,
+                "reason".to_string(),
+                false,
+                None,
+                None,
+            );
+            let mut buf = BytesMut::new();
+            subscribe_done.packetize(&mut buf);
+
+            let expected_bytes_array = [
+                0, // Subscribe ID (i)
+                0, // Status Code (i)
+                6, // Reason Phrase (b)
+                b'r', b'e', b'a', b's', b'o', b'n', // Reason Phrase (b)
+                0,    // Content Exists (b)
+            ];
+            assert_eq!(buf.as_ref(), expected_bytes_array.as_slice());
+        }
+        #[test]
+        fn depacketize() {
+            let bytes_array = [
+                0, // Subscribe ID (i)
+                0, // Status Code (i)
+                6, // Reason Phrase (b)
+                b'r', b'e', b'a', b's', b'o', b'n', // Reason Phrase (b)
+                0,    // Content Exists (b)
+            ];
+            let mut buf = BytesMut::new();
+            buf.extend_from_slice(&bytes_array);
+            let depacketized_subscribe_done = SubscribeDone::depacketize(&mut buf).unwrap();
+
+            let expected_subscribe_done = SubscribeDone::new(
+                0,
+                StatusCode::Unsubscribed,
+                "reason".to_string(),
+                false,
+                None,
+                None,
+            );
+
+            assert_eq!(depacketized_subscribe_done, expected_subscribe_done);
+        }
     }
 }
