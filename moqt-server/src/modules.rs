4672b153830c440c35fba7a2dc2c2ab7f20f4d8f
yuki-uchida <yuki.u.nrnwr@gmail.com>
2024-12-25 18:33:13 +0900

    Modify file names

--- moqt-server/src/modules.rs
+++ moqt-server/src/modules.rs
@@ -1,8 +1,8 @@
 pub mod buffer_manager;
+pub(crate) mod config;
 pub(crate) mod logging;
 pub(crate) mod message_handlers;
 pub(crate) mod moqt_client;
-pub(crate) mod moqt_config;
 pub(crate) mod object_cache_storage;
 pub(crate) mod pubsub_relation_manager;
 pub(crate) mod send_stream_dispatcher;
