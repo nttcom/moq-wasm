2e3158c9ed636d0638c0b28ba65d7a4b714fb9b5
yuki-uchida <yuki.u.nrnwr@gmail.com>
2025-02-25 16:35:16 +0900

    implement send moqt message

--- moqt-core/src/modules/constants.rs
+++ moqt-core/src/modules/constants.rs
@@ -1,7 +1,7 @@
 use num_enum::IntoPrimitive;
 
 // for draft-ietf-moq-transport-06
-pub const MOQ_TRANSPORT_VERSION: u32 = 0xff000006;
+pub const MOQ_TRANSPORT_VERSION: u32 = 0xff000008;
 
 #[derive(Debug, IntoPrimitive, PartialEq, Clone, Copy)]
 #[repr(u8)]
