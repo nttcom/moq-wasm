269e518a95a0f3ad8f883393100d5fa00c6c14ef
tetter27 <tetta.maeda@ntt.com>
2025-03-14 08:15:12 +0900

    feat: Relay supports OBJECT_DATAGRAM_STATUS message

--- moqt-server/src/modules/message_handlers/datagram_object.rs
+++ moqt-server/src/modules/message_handlers/datagram_object.rs
@@ -3,6 +3,8 @@ use crate::modules::moqt_client::MOQTClient;
 use crate::modules::moqt_client::MOQTClientStatus;
 use anyhow::{bail, Result};
 use bytes::{Buf, BytesMut};
+use moqt_core::messages::data_streams::datagram_status;
+use moqt_core::messages::data_streams::DatagramObject;
 use moqt_core::{
     data_stream_type::DataStreamType,
     messages::data_streams::{datagram, DataStreams},
@@ -14,7 +16,7 @@ use tokio::sync::Mutex;
 
 #[derive(Debug, PartialEq)]
 pub enum DatagramObjectProcessResult {
-    Success(datagram::Object),
+    Success(DatagramObject),
     Continue,
     Failure(TerminationErrorCode, String),
 }
@@ -29,6 +31,26 @@ fn read_data_stream_type(read_cur: &mut std::io::Cursor<&[u8]>) -> Result<DataSt
     let data_stream_type = DataStreamType::try_from(type_value)
         .map_err(|err| anyhow::anyhow!("Failed to convert value: {}", err))?;
     Ok(data_stream_type)
+}
+
+fn depacketize_object_datagram(read_cur: &mut std::io::Cursor<&[u8]>) -> Result<DatagramObject> {
+    match datagram::Object::depacketize(read_cur) {
+        Ok(object) => Ok(DatagramObject::ObjectDatagram(object)),
+        Err(err) => {
+            bail!(err.to_string());
+        }
+    }
+}
+
+fn depacketize_object_datagram_status(
+    read_cur: &mut std::io::Cursor<&[u8]>,
+) -> Result<DatagramObject> {
+    match datagram_status::Object::depacketize(read_cur) {
+        Ok(object) => Ok(DatagramObject::ObjectDatagramStatus(object)),
+        Err(err) => {
+            bail!(err.to_string());
+        }
+    }
 }
 
 pub(crate) async fn try_read_object(
@@ -69,8 +91,9 @@ pub(crate) async fn try_read_object(
         }
     };
 
-    let result = match data_stream_type {
-        DataStreamType::ObjectDatagram => datagram::Object::depacketize(&mut read_cur),
+    let depacketize_result = match data_stream_type {
+        DataStreamType::ObjectDatagram => depacketize_object_datagram(&mut read_cur),
+        DataStreamType::ObjectDatagramStatus => depacketize_object_datagram_status(&mut read_cur),
         _ => {
             return DatagramObjectProcessResult::Failure(
                 TerminationErrorCode::ProtocolViolation,
@@ -78,7 +101,8 @@ pub(crate) async fn try_read_object(
             );
         }
     };
-    match result {
+
+    match depacketize_result {
         Ok(object) => {
             buf.advance(read_cur.position() as usize);
             DatagramObjectProcessResult::Success(object)
@@ -103,7 +127,7 @@ mod tests {
         use bytes::BytesMut;
         use moqt_core::{
             data_stream_type::DataStreamType,
-            messages::data_streams::{datagram, DataStreams},
+            messages::data_streams::{datagram, datagram_status, DataStreams, DatagramObject},
             variable_integer::write_variable_integer,
         };
         use std::{io::Cursor, sync::Arc};
@@ -139,13 +163,51 @@ mod tests {
             let mut buf_without_type = BytesMut::with_capacity(bytes_array.len());
             buf_without_type.extend_from_slice(&bytes_array);
             let mut read_cur = Cursor::new(&buf_without_type[..]);
-            let datagram_object = datagram::Object::depacketize(&mut read_cur);
-
-            println!("{:?}", datagram_object);
+            let datagram_object = datagram::Object::depacketize(&mut read_cur).unwrap();
+            let datagram_object = DatagramObject::ObjectDatagram(datagram_object);
 
             assert_eq!(
                 result,
-                DatagramObjectProcessResult::Success(datagram_object.unwrap())
+                DatagramObjectProcessResult::Success(datagram_object)
+            );
+        }
+
+        #[tokio::test]
+        async fn datagram_object_status_success() {
+            let data_stream_type = DataStreamType::ObjectDatagramStatus;
+            let bytes_array = [
+                1, // Track Alias (i)
+                2, // Group ID (i)
+                3, // Object ID (i)
+                4, // Subscriber Priority (8)
+                0, // Extension Headers Length (i)
+                3, // Object Payload Length (i)
+                1, // Object Status (i)
+            ];
+            let mut buf = BytesMut::with_capacity(bytes_array.len() + 8);
+            buf.extend(write_variable_integer(data_stream_type as u64));
+            buf.extend_from_slice(&bytes_array);
+
+            let senders_mock = senders::test_helper_fn::create_senders_mock();
+            let upstream_session_id = 0;
+
+            let mut client = MOQTClient::new(upstream_session_id, senders_mock);
+            client.update_status(MOQTClientStatus::SetUp);
+            let client = Arc::new(Mutex::new(client));
+
+            let result = try_read_object(&mut buf, client).await;
+
+            println!("{:?}", result);
+
+            let mut buf_without_type = BytesMut::with_capacity(bytes_array.len());
+            buf_without_type.extend_from_slice(&bytes_array);
+            let mut read_cur = Cursor::new(&buf_without_type[..]);
+            let datagram_object = datagram_status::Object::depacketize(&mut read_cur).unwrap();
+            let datagram_object = DatagramObject::ObjectDatagramStatus(datagram_object);
+
+            assert_eq!(
+                result,
+                DatagramObjectProcessResult::Success(datagram_object)
             );
         }
     }
--- moqt-server/src/modules/object_cache_storage/cache/datagram.rs
+++ moqt-server/src/modules/object_cache_storage/cache/datagram.rs
@@ -1,11 +1,11 @@
 use super::CacheId;
-use moqt_core::messages::data_streams::datagram;
+use moqt_core::messages::data_streams::DatagramObject;
 use std::time::Duration;
 use ttl_cache::TtlCache;
 
 #[derive(Clone)]
 pub(crate) struct DatagramCache {
-    objects: TtlCache<CacheId, datagram::Object>,
+    objects: TtlCache<CacheId, DatagramObject>,
     next_cache_id: CacheId,
 }
 
@@ -19,7 +19,7 @@ impl DatagramCache {
         }
     }
 
-    pub(crate) fn insert_object(&mut self, object: datagram::Object, duration: u64) {
+    pub(crate) fn insert_object(&mut self, object: DatagramObject, duration: u64) {
         let ttl = Duration::from_millis(duration);
         self.objects.insert(self.next_cache_id, object, ttl);
         self.next_cache_id += 1;
@@ -29,9 +29,19 @@ impl DatagramCache {
         &mut self,
         group_id: u64,
         object_id: u64,
-    ) -> Option<(CacheId, datagram::Object)> {
+    ) -> Option<(CacheId, DatagramObject)> {
         self.objects.iter().find_map(|(k, v)| {
-            if v.group_id() == group_id && v.object_id() == object_id {
+            let g_id = match v {
+                DatagramObject::ObjectDatagram(obj) => obj.group_id(),
+                DatagramObject::ObjectDatagramStatus(obj) => obj.group_id(),
+            };
+
+            let o_id = match v {
+                DatagramObject::ObjectDatagram(obj) => obj.object_id(),
+                DatagramObject::ObjectDatagramStatus(obj) => obj.object_id(),
+            };
+
+            if g_id == group_id && o_id == object_id {
                 Some((*k, v.clone()))
             } else {
                 None
@@ -42,7 +52,7 @@ impl DatagramCache {
     pub(crate) fn get_next_object_with_cache_id(
         &mut self,
         cache_id: CacheId,
-    ) -> Option<(CacheId, datagram::Object)> {
+    ) -> Option<(CacheId, DatagramObject)> {
         let next_cache_id = cache_id + 1;
         self.objects.iter().find_map(|(k, v)| {
             if *k == next_cache_id {
@@ -53,35 +63,49 @@ impl DatagramCache {
         })
     }
 
-    pub(crate) fn get_latest_group_with_cache_id(&mut self) -> Option<(CacheId, datagram::Object)> {
+    pub(crate) fn get_latest_group_with_cache_id(&mut self) -> Option<(CacheId, DatagramObject)> {
         let latest_group_id = self
             .objects
             .iter()
             .last()
-            .map(|(_, v)| v.group_id())
+            .map(|(_, v)| match v {
+                DatagramObject::ObjectDatagram(obj) => obj.group_id(),
+                DatagramObject::ObjectDatagramStatus(obj) => obj.group_id(),
+            })
             .unwrap();
 
         let latest_group = self.objects.iter().filter_map(|(k, v)| {
-            if v.group_id() == latest_group_id {
+            let g_id = match v {
+                DatagramObject::ObjectDatagram(obj) => obj.group_id(),
+                DatagramObject::ObjectDatagramStatus(obj) => obj.group_id(),
+            };
+            if g_id == latest_group_id {
                 Some((*k, v.clone()))
             } else {
                 None
             }
         });
 
-        latest_group.min_by_key(|(k, v)| (v.object_id(), *k))
+        latest_group.min_by_key(|(k, v)| {
+            let o_id = match v {
+                DatagramObject::ObjectDatagram(obj) => obj.object_id(),
+                DatagramObject::ObjectDatagramStatus(obj) => obj.object_id(),
+            };
+            (o_id, *k)
+        })
     }
 
-    pub(crate) fn get_latest_object_with_cache_id(
-        &mut self,
-    ) -> Option<(CacheId, datagram::Object)> {
+    pub(crate) fn get_latest_object_with_cache_id(&mut self) -> Option<(CacheId, DatagramObject)> {
         self.objects.iter().last().map(|(k, v)| (*k, v.clone()))
     }
 
     pub(crate) fn get_largest_group_id(&mut self) -> u64 {
         self.objects
             .iter()
-            .map(|(_, v)| v.group_id())
+            .map(|(_, v)| match v {
+                DatagramObject::ObjectDatagram(obj) => obj.group_id(),
+                DatagramObject::ObjectDatagramStatus(obj) => obj.group_id(),
+            })
             .max()
             .unwrap()
     }
@@ -92,8 +116,18 @@ impl DatagramCache {
         self.objects
             .iter()
             .filter_map(|(_, v)| {
-                if v.group_id() == largest_group_id {
-                    Some(v.object_id())
+                let g_id = match v {
+                    DatagramObject::ObjectDatagram(obj) => obj.group_id(),
+                    DatagramObject::ObjectDatagramStatus(obj) => obj.group_id(),
+                };
+
+                let o_id = match v {
+                    DatagramObject::ObjectDatagram(obj) => obj.object_id(),
+                    DatagramObject::ObjectDatagramStatus(obj) => obj.object_id(),
+                };
+
+                if g_id == largest_group_id {
+                    Some(o_id)
                 } else {
                     None
                 }
