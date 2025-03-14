ff9a087b75e6746e308b64986b37271cb878ddc5
yuki-uchida <yuki.u.nrnwr@gmail.com>
2025-03-15 00:31:54 +0700

    Change chrome start script

--- scripts/start-localhost-test-chrome.sh
+++ scripts/start-localhost-test-chrome.sh
@@ -5,7 +5,6 @@
 # LICENSE file in the root directory of this source tree.
 
 # Get base 64 of scert cmd
-certbase64=$(eval "openssl x509 -pubkey -noout -in ./moqt-server-sample/keys/cert.pem | openssl rsa -pubin -outform der | openssl dgst -sha256 -binary | base64")
-
-# /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --test-type --origin-to-force-quic-on=localhost:4433 --ignore-certificate-errors-spki-list=$certbase64 --use-fake-device-for-media-stream
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --test-type --origin-to-force-quic-on=localhost:4433 --ignore-certificate-errors-spki-list=$certbase64
+certbase64=$(eval "openssl x509 -pubkey -noout -in ./moqt-server-sample/keys/cert.pem | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64")
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --test-type --origin-to-force-quic-on=35.189.95.121:4433,localhost:4433 --allow-insecure-localhost --ignore-certificate-errors --ignore-certificate-errors-spki-list=$certbase64
+# /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --test-type --origin-to-force-quic-on=35.189.95.121:4433,localhost:4433 --allow-insecure-localhost --ignore-certificate-errors --ignore-certificate-errors-spki-list=$certbase64 --use-fake-device-for-media-stream
