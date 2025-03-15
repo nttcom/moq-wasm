de236594296b794be69efbdd49e6eae23fdacd97
yuki-uchida <yuki.u.nrnwr@gmail.com>
2025-03-15 13:28:05 +0700

    Change start Chrome script

--- scripts/start-localhost-test-chrome.sh
+++ scripts/start-localhost-test-chrome.sh
@@ -6,5 +6,5 @@
 
 # Get base 64 of scert cmd
 certbase64=$(eval "openssl x509 -pubkey -noout -in ./moqt-server-sample/keys/cert.pem | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64")
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --test-type --origin-to-force-quic-on=35.189.95.121:4433,localhost:4433 --allow-insecure-localhost --ignore-certificate-errors --ignore-certificate-errors-spki-list=$certbase64
-# /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --test-type --origin-to-force-quic-on=35.189.95.121:4433,localhost:4433 --allow-insecure-localhost --ignore-certificate-errors --ignore-certificate-errors-spki-list=$certbase64 --use-fake-device-for-media-stream
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --test-type --origin-to-force-quic-on=35.189.95.121:4433,localhost:4433 --ignore-certificate-errors-spki-list=$certbase64
+# /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --test-type --origin-to-force-quic-on=35.189.95.121:4433,localhost:4433 --ignore-certificate-errors --ignore-certificate-errors-spki-list=$certbase64 --use-fake-device-for-media-stream
