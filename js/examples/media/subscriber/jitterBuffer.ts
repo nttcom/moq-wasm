38683af4c0d4ed48209ddbf343c2289304f0c53b
tetter27 <tetta.maeda@ntt.com>
2025-05-28 10:02:38 +0900

    chore: rename variables

--- js/examples/media/subscriber/jitterBuffer.ts
+++ js/examples/media/subscriber/jitterBuffer.ts
@@ -13,14 +13,11 @@ type JitterBufferEntry<T> = {
  * When reordering, groupId takes precedence over objectID.
  */
 export class JitterBuffer<T> {
-  private _buffer: Array<JitterBufferEntry<T>>
-  private _min_delay_ms: number
-  private _max_buffer_size: number
+  private buffer: JitterBufferEntry<T>[] = []
 
-  constructor(min_delay_ms: number, max_buffer_size?: number) {
-    this._buffer = []
-    this._min_delay_ms = min_delay_ms
-    this._max_buffer_size = max_buffer_size ? max_buffer_size : DEFAULT_JITTER_BUFFER_SIZE
+  constructor(    
+    private readonly minDelayMs: number,
+    private readonly maxBufferSize: number = DEFAULT_JITTER_BUFFER_SIZE,) {
   }
 
   push(groupId: number, objectId: number, object: T) {
@@ -28,12 +25,12 @@ export class JitterBuffer<T> {
     const entry = { groupId, objectId, timestamp, object }
 
     const pos = this._findInsertPos(groupId, objectId)
-    this._buffer.splice(pos, 0, entry)
+    this.buffer.splice(pos, 0, entry)
 
     // remove the oldest element if the buffer is full
-    if (this._buffer.length > this._max_buffer_size) {
+    if (this.buffer.length > this.maxBufferSize) {
       console.warn('JitterBuffer is full, removing the oldest element')
-      this._buffer.shift()
+      this.buffer.shift()
     }
   }
 
@@ -41,25 +38,25 @@ export class JitterBuffer<T> {
    * @returns the oldest object in the buffer if it is older than the minimum delay, otherwise null.
    */
   pop(): T | null {
-    if (this._buffer.length === 0) {
+    if (this.buffer.length === 0) {
       return null
     }
 
-    const buffer = this._buffer[0]
+    const buffer = this.buffer[0]
     const delayMs = performance.now() - buffer.timestamp
 
-    if (delayMs < this._min_delay_ms) {
+    if (delayMs < this.minDelayMs) {
       return null
     }
 
-    this._buffer.shift()
+    this.buffer.shift()
     return buffer.object
   }
 
   private _findInsertPos(groupId: number, objectId: number): number {
     // Most of the time, the newest object will be received, so search from the end of the buffer
-    for (let i = this._buffer.length - 1; i >= 0; i--) {
-      const buffer = this._buffer[i]
+    for (let i = this.buffer.length - 1; i >= 0; i--) {
+      const buffer = this.buffer[i]
       if (buffer.groupId === groupId && buffer.objectId < objectId) {
         return i + 1
       }
