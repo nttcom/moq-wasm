ff9725dd3d953a6dda662d0f716e096d68971c62
t-gazzy <t.kaji@ntt.com>
2025-06-19 19:05:35 +0900

    feat(server): setup Endpoint for MoQ

--- moqt-server/src/lib.rs
+++ moqt-server/src/lib.rs
@@ -1,10 +1,13 @@
 use anyhow::{Context, Result, bail};
+use wtransport::tls::rustls::pki_types::pem::PemObject;
+use wtransport::tls::rustls;
+use wtransport::tls::rustls::pki_types::{CertificateDer, PrivateKeyDer};
 use std::net::SocketAddr;
 use std::{collections::HashMap, sync::Arc, time::Duration};
 use tokio::sync::{Mutex, mpsc, mpsc::Sender};
 use tokio::task;
 use tracing::{self, Instrument};
-use wtransport::quinn::{TransportConfig, VarInt};
+use wtransport::quinn::{self, TransportConfig, VarInt};
 use wtransport::{Endpoint, Identity, ServerConfig};
 mod modules;
 pub use modules::config::MOQTConfig;
@@ -57,10 +60,52 @@ impl MOQTServer {
     }
     pub async fn start(&self) -> Result<()> {
         init_logging(self.log_level.to_string());
-
-        if self.underlay != UnderlayType::WebTransport {
+        if self.underlay == UnderlayType::QUIC {
+            self.setup_for_quic().await
+        } else if self.underlay == UnderlayType::WebTransport {
+            self.setup_for_webtransport().await
+        } else {
             bail!("Underlay must be WebTransport, not {:?}", self.underlay);
         }
+    }
+
+    async fn setup_for_quic(&self) -> Result<()> {
+        let cert = vec![CertificateDer::from_pem_file(&self.cert_path).expect("failed to load cert file.")];
+        let key = PrivateKeyDer::from_pem_file(&self.key_path).expect("failed to load key file.");
+        
+        let mut server_crypto = rustls::ServerConfig::builder()
+            .with_no_client_auth()
+            .with_single_cert(cert, key)?;
+        let alpn = &[b"moq-00"];
+        server_crypto.alpn_protocols = alpn.iter().map(|&x| x.into()).collect();
+        server_crypto.key_log = Arc::new(rustls::KeyLogFile::new());
+
+        let mut server_config =
+            quinn::ServerConfig::with_crypto(Arc::new(quinn::crypto::rustls::QuicServerConfig::try_from(server_crypto)?));
+        let mut transport_config = TransportConfig::default();
+        let keep_alive_sec = std::time::Duration::from_secs(self.keep_alive_interval_sec);
+        transport_config.keep_alive_interval(Some(keep_alive_sec));
+        // 単方向ストリーム数を100000に設定
+        transport_config.max_concurrent_uni_streams(100000u32.into());
+        // initial_max_stream_data_uniと同義。デフォルトは65,536 バイト (64KB) 大きくするとACKを待たずに送信するため、輻輳が発生する可能性が高まる
+        transport_config.send_window(64 * 1024);
+        // パケロス判定して再送を要求するまでの時間(RTTの倍数)を指定する。小さくすると再送が増える Default(RFC推奨値): 1.125
+        // transport_config.time_threshold(1.5);
+        // パケロス判定して再送を要求するまでのパケット間隔を指定する。小さくすると再送が増える Default(RFC推奨値): 3
+        transport_config.packet_threshold(5);
+        transport_config.stream_receive_window(VarInt::from_u32(1024 * 1024)); // initial_max_stream_data_uniと同義。デフォルトは65,536 バイト (64KB)なので1MBにする
+
+        let transport_arc = Arc::new(transport_config);
+        server_config.transport_config(transport_arc);
+        let address = SocketAddr::from(([0, 0, 0, 0], self.port));
+
+        let endpoint = quinn::Endpoint::server(server_config, address)?;
+        tracing::info!("Server ready! for QUIC");
+
+        Ok(())
+    }
+
+    async fn setup_for_webtransport(&self) -> Result<()> {
         let mut transport_config = TransportConfig::default();
         // 単方向ストリーム数を100000に設定
         transport_config.max_concurrent_uni_streams(100000u32.into());
