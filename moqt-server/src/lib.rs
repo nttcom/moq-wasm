5df5b0efeb65113ca5e3f539866402231403d000
t-gazzy <t.kaji@ntt.com>
2025-07-02 14:18:42 +0900

    chore: move directory

--- moqt-server/src/lib.rs
+++ moqt-server/src/lib.rs
@@ -1,13 +1,11 @@
 use anyhow::{Context, Result, bail};
-use wtransport::tls::rustls::pki_types::pem::PemObject;
-use wtransport::tls::rustls;
-use wtransport::tls::rustls::pki_types::{CertificateDer, PrivateKeyDer};
 use std::net::SocketAddr;
 use std::{collections::HashMap, sync::Arc, time::Duration};
 use tokio::sync::{Mutex, mpsc, mpsc::Sender};
 use tokio::task;
 use tracing::{self, Instrument};
-use wtransport::quinn::{self, TransportConfig, VarInt};
+// use wtransport::quinn::congestion::NewRenoConfig;
+use wtransport::quinn::{TransportConfig, VarInt};
 use wtransport::{Endpoint, Identity, ServerConfig};
 mod modules;
 pub use modules::config::MOQTConfig;
@@ -60,135 +58,26 @@ impl MOQTServer {
     }
     pub async fn start(&self) -> Result<()> {
         init_logging(self.log_level.to_string());
-        if self.underlay == UnderlayType::QUIC {
-            self.setup_for_quic().await
-        } else if self.underlay == UnderlayType::WebTransport {
-            self.setup_for_webtransport().await
-        } else {
+
+        if self.underlay != UnderlayType::WebTransport {
             bail!("Underlay must be WebTransport, not {:?}", self.underlay);
         }
-    }
-
-    async fn setup_for_quic(&self) -> Result<()> {
-        let cert = vec![CertificateDer::from_pem_file(&self.cert_path).expect("failed to load cert file.")];
-        let key = PrivateKeyDer::from_pem_file(&self.key_path).expect("failed to load key file.");
-        
-        let mut server_crypto = rustls::ServerConfig::builder()
-            .with_no_client_auth()
-            .with_single_cert(cert, key)?;
-        let alpn = &[b"moq-00"];
-        server_crypto.alpn_protocols = alpn.iter().map(|&x| x.into()).collect();
-        server_crypto.key_log = Arc::new(rustls::KeyLogFile::new());
-
-        let mut server_config =
-            quinn::ServerConfig::with_crypto(Arc::new(quinn::crypto::rustls::QuicServerConfig::try_from(server_crypto)?));
         let mut transport_config = TransportConfig::default();
-        let keep_alive_sec = std::time::Duration::from_secs(self.keep_alive_interval_sec);
-        transport_config.keep_alive_interval(Some(keep_alive_sec));
         // 単方向ストリーム数を100000に設定
         transport_config.max_concurrent_uni_streams(100000u32.into());
-        // initial_max_stream_data_uniと同義。デフォルトは65,536 バイト (64KB) 大きくするとACKを待たずに送信するため、輻輳が発生する可能性が高まる
-        transport_config.send_window(64 * 1024);
+        // initial_max_stream_data_uniと同義。デフォルトは65,536 バイト (64KB) 大きくするとACKを待たずに送信するため、輻輳が発生する可能性が高まるが、小さいとACKを受け取るまで次が送れないため、スループットが低下する
+        // 64KBの場合、15Mbps(=1.875MB)送ろうとすると 64KB / 1.875MB = 約0.034秒 ≒ 34msでACKを受け取る必要がある。RTT=200msの場合、400KBは設定していないといけない
+        transport_config.send_window(1024 * 1024);
         // パケロス判定して再送を要求するまでの時間(RTTの倍数)を指定する。小さくすると再送が増える Default(RFC推奨値): 1.125
         // transport_config.time_threshold(1.5);
         // パケロス判定して再送を要求するまでのパケット間隔を指定する。小さくすると再送が増える Default(RFC推奨値): 3
         transport_config.packet_threshold(5);
+        transport_config.receive_window(VarInt::from_u32(10 * 1024 * 1024)); // 1コネクション10Streamあった場合に10MBのデータを受信できるようにする。
         transport_config.stream_receive_window(VarInt::from_u32(1024 * 1024)); // initial_max_stream_data_uniと同義。デフォルトは65,536 バイト (64KB)なので1MBにする
-
-        let transport_arc = Arc::new(transport_config);
-        server_config.transport_config(transport_arc);
-        let address = SocketAddr::from(([0, 0, 0, 0], self.port));
-
-        let endpoint = quinn::Endpoint::server(server_config, address)?;
-        tracing::info!("Server ready! for QUIC");
-
-        let (buffer_tx, mut buffer_rx) = mpsc::channel::<BufferCommand>(1024);
-        task::Builder::new()
-            .name("Buffer Manager")
-            .spawn(async move { buffer_manager(&mut buffer_rx).await })?;
-        let (pubsub_relation_tx, mut pubsub_relation_rx) =
-            mpsc::channel::<PubSubRelationCommand>(1024);
-        task::Builder::new()
-            .name("PubSub Relation Manager")
-            .spawn(async move { pubsub_relation_manager(&mut pubsub_relation_rx).await })?;
-        let (control_message_dispatch_tx, mut control_message_dispatch_rx) =
-            mpsc::channel::<ControlMessageDispatchCommand>(1024);
-        task::Builder::new()
-            .name("Control Message Dispatcher")
-            .spawn(
-                async move { control_message_dispatcher(&mut control_message_dispatch_rx).await },
-            )?;
-        let (signal_dispatch_tx, mut signal_dispatch_rx) =
-            mpsc::channel::<SignalDispatchCommand>(1024);
-        task::Builder::new()
-            .name("Signal Dispatcher")
-            .spawn(async move { signal_dispatcher(&mut signal_dispatch_rx).await })?;
-
-        let (object_cache_tx, mut object_cache_rx) =
-            mpsc::channel::<ObjectCacheStorageCommand>(1024);
-        task::Builder::new()
-            .name("Object Cache Storage")
-            .spawn(async move { object_cache_storage(&mut object_cache_rx).await })?;
-
-        let start_forwarder_txes: Arc<Mutex<HashMap<usize, SenderToOpenSubscription>>> =
-            Arc::new(Mutex::new(HashMap::new()));
-
-        for id in 0.. {
-            let sender_to_other_connection_thread =
-                SenderToOtherConnectionThread::new(start_forwarder_txes.clone());
-            let senders_to_management_thread = SendersToManagementThread::new(
-                buffer_tx.clone(),
-                pubsub_relation_tx.clone(),
-                control_message_dispatch_tx.clone(),
-                signal_dispatch_tx.clone(),
-                object_cache_tx.clone(),
-            );
-
-            let incoming_session = endpoint.accept().await;
-            let session_span = tracing::info_span!("Session", id);
-
-            // Create a thread for each session
-            task::Builder::new()
-                .name("WT Session Handler")
-                .spawn(async move {
-                    let mut session_handler = SessionHandler::init(
-                        sender_to_other_connection_thread,
-                        senders_to_management_thread,
-                        incoming_session,
-                    )
-                    .instrument(session_span.clone())
-                    .await
-                    .unwrap();
-
-                    match session_handler
-                        .start()
-                        .instrument(session_span.clone())
-                        .await
-                    {
-                        Ok(_) => {}
-                        Err(err) => {
-                            tracing::error!("{:#?}", err);
-                        }
-                    }
-
-                    let _ = session_handler.finish().instrument(session_span).await;
-                })?;
-        }
-
-        Ok(())
-    }
-
-    async fn setup_for_webtransport(&self) -> Result<()> {
-        let mut transport_config = TransportConfig::default();
-        // 単方向ストリーム数を100000に設定
-        transport_config.max_concurrent_uni_streams(100000u32.into());
-        // initial_max_stream_data_uniと同義。デフォルトは65,536 バイト (64KB) 大きくするとACKを待たずに送信するため、輻輳が発生する可能性が高まる
-        transport_config.send_window(64 * 1024);
-        // パケロス判定して再送を要求するまでの時間(RTTの倍数)を指定する。小さくすると再送が増える Default(RFC推奨値): 1.125
-        // transport_config.time_threshold(1.5);
-        // パケロス判定して再送を要求するまでのパケット間隔を指定する。小さくすると再送が増える Default(RFC推奨値): 3
-        transport_config.packet_threshold(5);
-        transport_config.stream_receive_window(VarInt::from_u32(1024 * 1024)); // initial_max_stream_data_uniと同義。デフォルトは65,536 バイト (64KB)なので1MBにする
+        transport_config.congestion_controller_factory(Arc::new(
+            wtransport::quinn::congestion::BbrConfig::default(),
+            // wtransport::quinn::congestion::NewRenoConfig::default(),
+        ));
 
         let mut config = ServerConfig::builder()
             .with_bind_address(SocketAddr::from(([0, 0, 0, 0], self.port)))
@@ -206,7 +95,7 @@ impl MOQTServer {
             )
             .keep_alive_interval(Some(Duration::from_secs(self.keep_alive_interval_sec)))
             .build();
-        let _ = config.quic_endpoint_config_mut().max_udp_payload_size(1280); // Default: 1472
+        let _ = config.quic_endpoint_config_mut().max_udp_payload_size(1472); // Default: 1472
 
         let server = Endpoint::server(config)?;
         tracing::info!("Server ready!");
