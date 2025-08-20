a163d37b513820459fb08a20b50e4e3b0c07768f
t-gazzy <t.kaji@ntt.com>
2025-08-20 20:17:21 +0900

    chore: renames

--- moqt-server-v2/src/modules/session_handlers/moqt_connection_factory.rs
+++ moqt-server-v2/src/modules/session_handlers/moqt_connection_factory.rs
@@ -3,6 +3,6 @@ use async_trait::async_trait;
 use crate::modules::session_handlers::moqt_connection::MOQTConnection;
 
 #[async_trait]
-pub(crate) trait MOQTConnectionCreator: Send + Sync {
+pub(crate) trait MOQTConnectionFactory: Send + Sync {
     async fn accept_new_connection(&mut self) -> anyhow::Result<Box<dyn MOQTConnection>>;
 }
