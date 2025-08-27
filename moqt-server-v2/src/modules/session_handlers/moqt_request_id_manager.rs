69ef5edf0e996061107aede4ba05c4cc6c938ae0
t-gazzy <t.kaji@ntt.com>
2025-08-27 22:06:58 +0900

    chore: tidy

--- moqt-server-v2/src/modules/session_handlers/moqt_request_id_manager.rs
+++
@@ -1,9 +0,0 @@
-pub(crate) struct MOQTRequestIdManager {
-    request_id: std::sync::atomic::AtomicU64
-}
-
-impl MOQTRequestIdManager {
-    pub(crate) fn get_next_request_id(&mut self) -> u64 {
-        self.request_id.load(std::sync::atomic::Ordering::Relaxed)
-    }
-}
