c7a6e7340432d1e0c653a44d6ef8298f5b96d067
t-gazzy <t.kaji@ntt.com>
2025-08-28 23:08:58 +0900

    feat: move method that calls setup in `MOQTConnection`

--- moqt/src/modules/moqt/moqt_connection.rs
+++ moqt/src/modules/moqt/moqt_connection.rs
@@ -1,3 +1,6 @@
+use crate::modules::moqt::constants;
+use crate::modules::moqt::messages::control_messages::setup_parameters::MaxSubscribeID;
+use crate::modules::moqt::messages::control_messages::setup_parameters::SetupParameter;
 use crate::modules::moqt::moqt_message_controller::MOQTMessageController;
 use crate::modules::transport::transport_connection::TransportConnection;
 
@@ -7,13 +10,33 @@ pub(crate) struct MOQTConnection {
 }
 
 impl MOQTConnection {
-    pub(crate) fn new(
+    pub(crate) async fn new(
+        is_client: bool,
         transport_connection: Box<dyn TransportConnection>,
         message_controller: MOQTMessageController,
-    ) -> Self {
-        Self {
+    ) -> anyhow::Result<Self> {
+        if is_client {
+            Self::setup_for_client(&message_controller).await?;
+        } else {
+            Self::setup_for_server(&message_controller).await?;
+        }
+        Ok(Self {
             transport_connection,
             message_controller,
-        }
+        })
+    }
+
+    async fn setup_for_client(message_controller: &MOQTMessageController) -> anyhow::Result<()> {
+        let max_id = MaxSubscribeID::new(1000);
+        message_controller
+            .client_setup(
+                vec![constants::MOQ_TRANSPORT_VERSION],
+                vec![SetupParameter::MaxSubscribeID(max_id)],
+            )
+            .await
+    }
+
+    async fn setup_for_server(message_controller: &MOQTMessageController) -> anyhow::Result<()> {
+        message_controller.server_setup().await
     }
 }
