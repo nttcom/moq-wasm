cabad75c90bcbca53019a2b5bcb352d5336264c6
t-gazzy <t.kaji@ntt.com>
2025-10-07 11:32:15 +0900

    feat: add logic for pubsub_namespace

--- sdk/server/src/modules/tables.rs
+++ sdk/server/src/modules/tables.rs
@@ -1,57 +1,22 @@
-use std::collections::{HashMap, HashSet};
-
+use dashmap::{DashMap, DashSet};
 use uuid::Uuid;
 type Namespace = String;
+type TrackNamespace = String;
 
 pub(crate) struct Tables {
-    publishers: tokio::sync::Mutex<HashMap<Namespace, HashSet<Uuid>>>,
-    subscribers: tokio::sync::Mutex<HashMap<Namespace, HashSet<Uuid>>>,
-    namespace_trackers: tokio::sync::Mutex<HashMap<Namespace, HashSet<Uuid>>>,
+    pub(crate) publishers: DashMap<Namespace, DashSet<Uuid>>,
+    pub(crate) subscribers: DashMap<Namespace, DashSet<Uuid>>,
+    pub(crate) publisher_namespaces: DashMap<Namespace, DashSet<TrackNamespace>>,
+    pub(crate) subscriber_namespaces: DashMap<Namespace, DashSet<TrackNamespace>>,
 }
 
 impl Tables {
     pub(crate) fn new() -> Self {
         Self {
-            table: tokio::sync::Mutex::new(HashMap::new()),
-        }
-    }
-
-    pub(crate) async fn add(&self, namespace: Namespace, uuid: Uuid) {
-        let mut table = self.table.lock().await;
-        if table.contains_key(&namespace) {
-            table.get_mut(&namespace).unwrap().insert(uuid);
-        } else {
-            let mut set = HashSet::new();
-            set.insert(uuid);
-            table.insert(namespace, set);
-        }
-    }
-
-    pub(crate) async fn get_by_namespace(&self, namespace: Namespace) -> HashSet<Uuid> {
-        let mut table = self.table.lock().await;
-        match table.get_mut(&namespace) {
-            Some(set) => set.clone(),
-            None => {
-                tracing::error!("namespace not found");
-                HashSet::new()
-            }
-        }
-    }
-
-    pub(crate) async fn remove(&self, namespace: Namespace, uuid: Uuid) {
-        let mut table = self.table.lock().await;
-        let set = match table.get_mut(&namespace) {
-            Some(v) => v,
-            None => {
-                tracing::error!("namespace not found");
-                return;
-            }
-        };
-        set.remove(&uuid);
-
-        if set.is_empty() {
-            tracing::info!("Namespace has no session. Good bye! {}", &namespace);
-            table.remove(&namespace);
+            publishers: DashMap::new(),
+            subscribers: DashMap::new(),
+            publisher_namespaces: DashMap::new(),
+            subscriber_namespaces: DashMap::new(),
         }
     }
 }
