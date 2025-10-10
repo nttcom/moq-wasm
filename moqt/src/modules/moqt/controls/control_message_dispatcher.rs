20153cba3eec17091a2bfd2465df2eedd38881af
t-gazzy <t.kaji@ntt.com>
2025-10-10 09:44:07 +0900

    fix: message is resolved non-parallel

--- moqt/src/modules/moqt/controls/control_message_dispatcher.rs
+++ moqt/src/modules/moqt/controls/control_message_dispatcher.rs
@@ -1,10 +1,6 @@
-use std::{
-    pin::Pin,
-    sync::{Arc, Weak},
-};
+use std::sync::{Arc, Weak};
 
 use bytes::BytesMut;
-use futures::{StreamExt, stream::FuturesUnordered};
 
 use crate::{
     TransportProtocol,
@@ -29,31 +25,27 @@ impl ControlMessageDispatcher {
         tokio::task::Builder::new()
             .name("Message Receiver")
             .spawn(async move {
-                let mut futs = FuturesUnordered::new();
                 loop {
                     if let Some(session) = inner_session.upgrade() {
                         tracing::debug!("Session is alive.");
-                        tokio::select! {
-                            bytes = session.receive_stream.receive() => {
-                                tracing::info!("Message has been received.");
-                                if let Err(e) = bytes {
-                                    tracing::error!("failed to receive message: {:?}", e);
-                                    break;
-                                }
-                                let mut bytes_mut = BytesMut::from(bytes.unwrap().as_slice());
-                                let message_type = get_message_type(&mut bytes_mut);
-                                if let Err(e) = message_type.as_ref() {
-                                    tracing::error!("Protocol violation: {:?}", e);
-                                    break;
-                                }
-                                let result = Self::resolve_message(session, message_type.unwrap(), bytes_mut);
-                                futs.push(result);
-                                tracing::debug!("futus has been pushed. length: {}", futs.len());
-                            },
-                            Some(_) = futs.next(), if !futs.is_empty() => {
-                                tracing::debug!("futus has been resolved.");
+
+                        let bytes = match session.receive_stream.receive().await {
+                            Ok(b) => b,
+                            Err(e) => {
+                                tracing::error!("failed to receive message: {:?}", e);
+                                break;
                             }
-                        }
+                        };
+                        tracing::info!("Message has been received.");
+                        let mut bytes_mut = BytesMut::from(bytes.as_slice());
+                        let message_type = match get_message_type(&mut bytes_mut) {
+                            Ok(m) => m,
+                            Err(e) => {
+                                tracing::error!("Protocol violation: {:?}", e);
+                                break;
+                            }
+                        };
+                        let result = Self::resolve_message(session, message_type, bytes_mut).await;
                     } else {
                         tracing::error!("Session has been dropped.");
                         break;
@@ -63,11 +55,11 @@ impl ControlMessageDispatcher {
             .unwrap()
     }
 
-    fn resolve_message<T: TransportProtocol>(
+    async fn resolve_message<T: TransportProtocol>(
         session: Arc<InnerSession<T>>,
         message_type: ControlMessageType,
         bytes_mut: BytesMut,
-    ) -> Pin<Box<dyn Future<Output = ()> + Send>> {
+    ) {
         tracing::debug!("message_type: {:?}", message_type);
         match message_type {
             ControlMessageType::GoAway => todo!(),
@@ -88,26 +80,26 @@ impl ControlMessageDispatcher {
             ControlMessageType::FetchCancel => todo!(),
             ControlMessageType::TrackStatusRequest => todo!(),
             ControlMessageType::TrackStatus => todo!(),
-            ControlMessageType::PublishNamespace => Box::pin(
-                PublishNamespaceHandler::publish_namespace(session, bytes_mut),
-            ),
-            ControlMessageType::PublishNamespaceOk => Box::pin(
-                PublishNamespaceHandler::publish_namespace_ok(session, bytes_mut),
-            ),
-            ControlMessageType::PublishNamespaceError => Box::pin(
-                PublishNamespaceHandler::publish_namespace_error(session, bytes_mut),
-            ),
+            ControlMessageType::PublishNamespace => {
+                PublishNamespaceHandler::publish_namespace(session, bytes_mut).await
+            }
+            ControlMessageType::PublishNamespaceOk => {
+                PublishNamespaceHandler::publish_namespace_ok(session, bytes_mut).await
+            }
+            ControlMessageType::PublishNamespaceError => {
+                PublishNamespaceHandler::publish_namespace_error(session, bytes_mut).await
+            }
             ControlMessageType::PublishNamespaceDone => todo!(),
             ControlMessageType::PublishNamespaceCancel => todo!(),
-            ControlMessageType::SubscribeNamespace => Box::pin(
-                SubscribeNamespaceHandler::subscribe_namespace(session, bytes_mut),
-            ),
-            ControlMessageType::SubscribeNamespaceOk => Box::pin(
-                SubscribeNamespaceHandler::subscribe_namespace_ok(session, bytes_mut),
-            ),
-            ControlMessageType::SubscribeNamespaceError => Box::pin(
-                SubscribeNamespaceHandler::subscribe_namespace_error(session, bytes_mut),
-            ),
+            ControlMessageType::SubscribeNamespace => {
+                SubscribeNamespaceHandler::subscribe_namespace(session, bytes_mut).await
+            }
+            ControlMessageType::SubscribeNamespaceOk => {
+                SubscribeNamespaceHandler::subscribe_namespace_ok(session, bytes_mut).await
+            }
+            ControlMessageType::SubscribeNamespaceError => {
+                SubscribeNamespaceHandler::subscribe_namespace_error(session, bytes_mut).await
+            }
             ControlMessageType::UnSubscribeNamespace => todo!(),
             _ => todo!(),
         }
