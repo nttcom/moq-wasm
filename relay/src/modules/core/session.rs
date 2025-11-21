210659c0ce8febc50c72461f361d438fdef6db4e
t-gazzy <t.kaji@ntt.com>
2025-11-21 17:19:33 +0900

    chore: remove unused api

--- relay/src/modules/core/session.rs
+++ relay/src/modules/core/session.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 use async_trait::async_trait;
 
 use crate::modules::core::{
@@ -9,7 +7,6 @@ use crate::modules::core::{
 #[async_trait]
 pub(crate) trait Session: 'static + Send + Sync {
     fn new_publisher_subscriber_pair(&self) -> (Box<dyn Publisher>, Box<dyn Subscriber>);
-    async fn get_subscribed_namespaces(&self) -> HashSet<String>;
     async fn receive_session_event(&self) -> anyhow::Result<SessionEvent>;
 }
 
@@ -18,10 +15,6 @@ impl<T: moqt::TransportProtocol> Session for moqt::Session<T> {
     fn new_publisher_subscriber_pair(&self) -> (Box<dyn Publisher>, Box<dyn Subscriber>) {
         let (publisher, subscriber) = self.create_publisher_subscriber_pair();
         (Box::new(publisher), Box::new(subscriber))
-    }
-
-    async fn get_subscribed_namespaces(&self) -> HashSet<String> {
-        self.get_subscribed_namespaces().await
     }
 
     async fn receive_session_event(&self) -> anyhow::Result<SessionEvent> {
