0ea30273176e87bf9d38b4aa207384e5e3eb581c
d-matsui <mti.daiki@gmail.com>
2026-03-06 17:56:34 +0900

    feat: add interop example with moqtail publisher and Shaka Player subscriber

---
+++ examples/interop-moqtail-shaka/publisher/src/connection.ts
@@ -0,0 +1,53 @@
+declare const __CERT_HASH_BASE64__: string;
+
+import { MOQtailClient } from "moqtail";
+
+function decodeCertHash(base64: string): Uint8Array {
+	const binary = atob(base64);
+	const bytes = new Uint8Array(binary.length);
+	for (let i = 0; i < binary.length; i++) {
+		bytes[i] = binary.charCodeAt(i);
+	}
+	return bytes;
+}
+
+export type MessageCallback = (msg: unknown) => void;
+
+export async function createClient(opts?: {
+	onMessageSent?: MessageCallback;
+	onMessageReceived?: MessageCallback;
+}): Promise<MOQtailClient> {
+	const url = "https://localhost:4434";
+	const certHash = decodeCertHash(__CERT_HASH_BASE64__);
+	console.log("Connecting via WebTransport to", url);
+	console.log("Cert hash:", __CERT_HASH_BASE64__);
+
+	const client = await MOQtailClient.new({
+		url,
+		supportedVersions: [0xff00000e],
+		enableDatagrams: false,
+		transportOptions: {
+			serverCertificateHashes: [
+				{
+					algorithm: "sha-256",
+					value: certHash.buffer,
+				},
+			],
+		},
+		callbacks: {
+			onSessionTerminated: (reason) => {
+				console.warn("Session terminated:", reason);
+			},
+			onMessageSent: (msg) => {
+				console.log(`[sent] ${msg.constructor.name}`, msg);
+				opts?.onMessageSent?.(msg);
+			},
+			onMessageReceived: (msg) => {
+				console.log(`[recv] ${msg.constructor.name}`, msg);
+				opts?.onMessageReceived?.(msg);
+			},
+		},
+	});
+
+	return client;
+}
