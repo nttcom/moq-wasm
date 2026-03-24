ddc3ceaa02340d31910850747c1d8e657edbe00a
t-gazzy <t.kaji@ntt.com>
2026-03-25 00:22:57 +0900

    fix: remove joinset

--- relay/src/modules/session_handler.rs
+++ relay/src/modules/session_handler.rs
@@ -32,42 +32,32 @@ impl SessionHandler {
             .spawn(async move {
                 loop {
                     tracing::info!("accepting...");
-                    let mut join_set = tokio::task::JoinSet::new();
-                    tokio::select! {
-                        connecting = endpoint.accept() => {
-                            let connecting = match connecting {
-                                Ok(s) => s,
-                                Err(_) => {
-                                    tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-                                    break;
-                                }
-                            };
-                            join_set.spawn(connecting);
-                        },
-                        result = join_set.join_next() => {
-                            tracing::info!("a session task finished.");
-                            match result {
-                                Some(Ok(connecting)) => {
-                                    let session = match connecting {
-                                        Ok(s) => s,
-                                        Err(e) => {
-                                            tracing::error!("failed to negotiate: {}", e);
-                                            continue;
-                                        }
-                                    };
-                                    let session_id = generate_session_id();
-                                    tracing::info!("Session ID: {}", session_id);
-                                    repo.lock()
-                                        .await
-                                        .add(session_id, Box::new(session), session_event_sender.clone())
-                                        .await;
-                                },
-                                _ => {
-                                    tracing::info!("no more session tasks.");
-                                }
-                            }
+                    let connecting = match endpoint.accept().await {
+                        Ok(c) => c,
+                        Err(e) => {
+                            tracing::error!("failed to accept connection: {}", e);
+                            tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+                            continue;
                         }
-                    }
+                    };
+                    let cloned_repo = repo.clone();
+                    let session_event_sender = session_event_sender.clone();
+                    tokio::spawn(async move {
+                        let session = match connecting.await {
+                            Ok(s) => s,
+                            Err(e) => {
+                                tracing::error!("failed to negotiate: {}", e);
+                                return;
+                            }
+                        };
+                        let session_id = generate_session_id();
+                        tracing::info!("Session ID: {}", session_id);
+                        cloned_repo
+                            .lock()
+                            .await
+                            .add(session_id, Box::new(session), session_event_sender.clone())
+                            .await;
+                    });
                 }
             })
             .unwrap()
