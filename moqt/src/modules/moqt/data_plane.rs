ea42e1d5dba91e0464902fc9120aa67a215dcb6e
t-gazzy <t.kaji@ntt.com>
2026-05-20 20:20:56 +0900

    chore: add cfg

--- moqt/src/modules/moqt/data_plane.rs
+++ moqt/src/modules/moqt/data_plane.rs
@@ -1,5 +1,6 @@
 #[cfg(not(target_arch = "wasm32"))]
 pub(crate) mod codec;
+#[cfg(not(target_arch = "wasm32"))]
 pub(crate) mod datagram;
 pub(crate) mod object;
 #[cfg(not(target_arch = "wasm32"))]
