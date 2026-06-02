3b23a46414f62935b07df91fff11f34ecab1499a
yuki-uchida <yuki.u.nrnwr@gmail.com>
2026-06-02 16:31:42 +0900

    allow: too_many_arguments

--- relay/src/modules/event_handler.rs
+++ relay/src/modules/event_handler.rs
@@ -34,6 +34,7 @@ pub(crate) struct EventHandler {
 }
 
 impl EventHandler {
+    #[allow(clippy::too_many_arguments)]
     pub(crate) fn run(
         repo: Arc<tokio::sync::Mutex<SessionRepository>>,
         relay_event_receiver: tokio::sync::mpsc::UnboundedReceiver<SessionEvent>,
@@ -59,6 +60,7 @@ impl EventHandler {
         }
     }
 
+    #[allow(clippy::too_many_arguments)]
     fn create_relay_session_event_handler(
         repo: Arc<tokio::sync::Mutex<SessionRepository>>,
         mut receiver: tokio::sync::mpsc::UnboundedReceiver<SessionEvent>,
